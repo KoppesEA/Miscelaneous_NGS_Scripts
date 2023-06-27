f1a2f813810333df2695b3faf8c301c3c5559c98
Erik Koppes <41686327+KoppesEA@users.noreply.github.com>
2023-06-27 13:55:53 -0400

    Update and rename SRX_IdentifierMetadata.awk to SRX_IdentifierMetadata.sh

--- SRX_IdentifierMetadata.sh
+++ SRX_IdentifierMetadata.sh
@@ -1,5 +1,6 @@
-# SRX identifer AWK one-liner by Koppes EA
+# SRX identifer GREP one-liner by Koppes EA
 
 # Example metadata from PRJNA855125 (https://www.ncbi.nlm.nih.gov/bioproject/PRJNA855125) via the SRA run selector
 # Extraction of SRX Identifiers from SRA metadata tables into SRX_list.csv for use with nf-core fetching 1.8
+# note may have to manually inspect SraRunTable and set digits (rather than 8) to match SRX records for specific projects
 grep -o -E "SRX\d{8}" PRJNA855125_SraRunTable.txt > PRJNA855125_SRX_list.csv
